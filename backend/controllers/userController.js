3ee6b93cfab259367b09f76e7fb4fc3c451bb089
Eric <erickims08@gmail.com>
2020-11-14 14:59:19 +0300

    request for registering users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,15 +44,16 @@ export const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    res
-      .status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
   } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 });
 
