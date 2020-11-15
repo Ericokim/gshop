b6efc76f1e93de78d649599a6d4db436e8243833
Eric <erickims08@gmail.com>
2020-11-15 15:26:02 +0300

    Added User Registration functionality to the frontend

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -11,20 +11,11 @@ export const productListReducer = (state = { products: [] }, action) => {
   const { type, payload } = action;
   switch (type) {
     case PRODUCT_LIST_REQUEST:
-      return {
-        loading: true,
-        products: [],
-      };
+      return { loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
-      return {
-        loading: false,
-        products: payload,
-      };
+      return { loading: false, products: payload };
     case PRODUCT_LIST_FAIL:
-      return {
-        loading: false,
-        error: payload,
-      };
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
@@ -39,15 +30,9 @@ export const productDetailsReducer = (
     case PRODUCT_DETAILS_REQUEST:
       return { ...state, loading: true };
     case PRODUCT_DETAILS_SUCCESS:
-      return {
-        loading: false,
-        product: payload,
-      };
+      return { loading: false, product: payload };
     case PRODUCT_DETAILS_FAIL:
-      return {
-        loading: false,
-        error: payload,
-      };
+      return { loading: false, error: payload };
     default:
       return state;
   }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,27 +3,36 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
   const { type, payload } = action;
   switch (type) {
     case USER_LOGIN_REQUEST:
-      return {
-        loading: true,
-      };
+      return { loading: true };
     case USER_LOGIN_SUCCESS:
-      return {
-        loading: false,
-        userInfo: payload,
-      };
+      return { loading: false, userInfo: payload };
     case USER_LOGIN_FAIL:
-      return {
-        loading: false,
-        error: payload,
-      };
+      return { loading: false, error: payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: payload };
     default:
       return state;
   }
