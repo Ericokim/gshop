4f8b22de4d6e0ba4ddb34b30808b21f076614c84
Eric <erickims08@gmail.com>
2021-10-22 19:19:11 +0300

    feat(ProductsController): :zap: Backend Create and Update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,8 +31,72 @@ export const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    await product.remove()
+    await product.remove();
     res.json({ message: "Product removed successfully" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    image,
+    brand,
+    category,
+    description,
+    price,
+    countInStock,
+    numReviews,
+  } = req.body;
+
+  // const productExists = await Product.findOne({ name });
+
+  // if (productExists) {
+  //   res.status(400);
+  //   throw new Error("Product already exists");
+  // }
+
+  const product = new Product({
+    user: req.user._id,
+    name,
+    image,
+    brand,
+    category,
+    description,
+    price,
+    countInStock,
+    numReviews,
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error("Product not found");
